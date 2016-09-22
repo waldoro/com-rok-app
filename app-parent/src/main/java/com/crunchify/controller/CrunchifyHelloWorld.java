68e7466844841561defbd3f40f375a050525ca01
waldekr <waldek.r@gmail.com>
2016-09-22 14:53:19 +0200

    changes

--- app-parent/src/main/java/com/crunchify/controller/CrunchifyHelloWorld.java
+++ app-parent/src/main/java/com/crunchify/controller/CrunchifyHelloWorld.java
@@ -4,11 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
  
-/*
- * author: Crunchify.com
- * 
- */
- 
 @Controller
 public class CrunchifyHelloWorld {
  
